05ca77a330612310321eb9f15063eac4a0f9c5c1
felipefronck <fronckowiakfelipe@gmail.com>
2024-11-04 09:43:15 -0300

    verificação na main para tipo de dispositivo que es´ta rodando o app

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
-  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
-    sqfliteFfiInit();
-    databaseFactory = databaseFactoryFfi;
+  if (!kIsWeb) {
+    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+      sqfliteFfiInit();
+      databaseFactory = databaseFactoryFfi; // Inicializa apenas para desktop
+    }
   }
-
   runApp(const Appwidget());
 }
